e17619185a69eda2e2fb1fc0e3ef42c8c4c26b89
Raquel Gutiérrez Martín <raquel.guti8@gmail.com>
2020-09-24 20:24:54 +0200

    Editar animal

--- src/main/java/com/init/mascotas/controller/AnimalController.java
+++ src/main/java/com/init/mascotas/controller/AnimalController.java
@@ -9,6 +9,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -68,5 +69,12 @@ public class AnimalController {
 		@RequestParam(required = false) Boolean adoptado, @RequestParam(required = false) String sexo) {
 	
 	return animalRepository.findByAllAtributtes(tipo, raza, edad, tipoEdad, adoptado, sexo);
+	}
+	// Editar animal
+	@PutMapping(value="/editarAnimal/{id}")
+	@CrossOrigin(origins = "http://localhost:4200")
+	public Animal editarAnimal(@PathVariable("id") Integer id, @Valid @RequestBody Animal animal) {
+		animal.setIdAnimal(id);
+	    return animalRepository.save(animal);
 	}
 }
