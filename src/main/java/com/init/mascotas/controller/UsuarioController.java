737d54f4b473fcba99ee3f55e478ed5e3b9055e3
Raquel Gutiérrez Martín <raquel.guti8@gmail.com>
2020-11-14 11:05:13 +0100

    Mejoras registrar usuario

--- src/main/java/com/init/mascotas/controller/UsuarioController.java
+++ src/main/java/com/init/mascotas/controller/UsuarioController.java
@@ -1,10 +1,13 @@
 package com.init.mascotas.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
+import javax.sql.rowset.serial.SerialException;
 import javax.validation.Valid;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.init.mascotas.entities.Animal;
 import com.init.mascotas.entities.Usuario;
 import com.init.mascotas.repository.UsuarioRepository;
 
@@ -48,38 +52,14 @@ public class UsuarioController {
     public List<Usuario> usuarioPorEmailPass(@PathVariable(value="email") String email, @PathVariable(value="pass") String pass){
         return usuarioRepository.usuarioPorEmailPass(email, pass);
     }
-	// Crea un usuario
-	@PostMapping("/crearUsuario/{nombre}/{apellidos}/{email}/{pass}/{sexo}/{dni}/{edad}/{direccion}/{provincia}/{codigoPostal}/{telefono}")
+	// Guardar usuario
 	@CrossOrigin(origins = "http://localhost:4200")
-	Usuario crearUsuario(
-			@PathVariable(value="nombre") String nombre,
-			@PathVariable(value="apellidos") String apellidos, 
-			@PathVariable(value="email") String email,
-			@PathVariable(value="pass") String pass,
-			@PathVariable(value="sexo") String sexo,
-			@PathVariable(value="dni") String dni, 
-			@PathVariable(value="edad") int edad,
-			@PathVariable(value="direccion") String direccion,
-			@PathVariable(value="provincia")String provincia,
-			@PathVariable(value="codigoPostal") String codigoPostal,
-			@PathVariable(value="telefono")String telefono) {
-		
-		Usuario user =  new Usuario();
-		user.setIdUsuario(this.obtenerUltimoId() + 1);
-		user.setApellidos(apellidos);
-		user.setNombre(nombre);
-		user.setEmail(email);
-		user.setPassword(pass);
-		user.setSexo(sexo);
-		user.setDni(dni);
-		user.setEdad(edad);
-		user.setDireccion(direccion);
-		user.setProvincia(provincia);
-		user.setCodigoPostal(codigoPostal);
-		user.setTelefono(telefono);
-		return usuarioRepository.save(user);
-		// return (Usuario) usuarioRepository.crearUsuario(nombre, apellidos, email, pass, sexo, dni, edad, direccion, provincia, codigoPostal, telefono);
-	}
+	@PostMapping("/guardarUsuario")
+    public Usuario guardarUsuario(@RequestBody Usuario usuario) throws SerialException, SQLException{
+		System.out.println(this.obtenerUltimoId() + 1);
+		usuario.setIdUsuario(this.obtenerUltimoId() + 1);
+		return usuarioRepository.save(usuario);
+    }
 	// Último id
 	@GetMapping("/ultimoId")
 	public int obtenerUltimoId() {
